ae76eddfc628d98ae86aaff13a1c1d6d10db2bf0
CarlosAndrés <carlos.andres_paez@outlook.com>
2021-04-27 23:25:54 -0500

    Primer avance de las entidades

---
+++ RPG/Personajes/Entity.java
@@ -0,0 +1,16 @@
+package Personajes;
+
+public abstract class Entity {
+	
+	private int hp;
+	private int mp;
+	private int lvl;
+	private String status;
+	private int strenght;
+	private int wisdom;
+	private int dexterity;
+	
+	
+	public abstract void useSkill();
+	
+}
