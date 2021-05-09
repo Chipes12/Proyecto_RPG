91b189bf9fae7148daa6b9d3e6fada125e2462f9
SofiaVA43 <sofia-vargas2001@hotmail.com>
2021-05-09 18:34:47 -0500

    Se actualizó Shop

--- RPG/src/rpg/items/Shop.java
+++ RPG/src/rpg/items/Shop.java
@@ -2,27 +2,31 @@ package rpg.items;
 import java.util.*;
 
 public class Shop {
-
 	private TreeSet<Item> items;
 	
 	//Constructor
 	public Shop () {
 	}
+	
 	//Getter
 	public TreeSet<Item> getItems() {
 		return items;
 	}
+	
 	//Setter
 	public void setItems(TreeSet<Item> items) {
 		this.items = items;
 	}
+	
 	//Methods
 	public void addItem(Item item) {
 		this.items.add(item);
 	}
+	
 	public void deleteItem(Item item) {
 		this.items.remove(item);
 	}
+	
 	public String toString() {
 		String str = "************Shop*************\t\n";
 		List<Item> itemsList = new ArrayList<Item>(items);
@@ -31,6 +35,7 @@ public class Shop {
 		}
 		return str;
 	}
+	
 	public Item selectItem(int index) {
 		List<Item> itemsList = new ArrayList<Item>(items);
 		return itemsList.get(index - 1);
