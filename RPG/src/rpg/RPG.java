d77cb1e1aa7ea4808d66c8edd1df2f77f264ea09
CarlosAndrés <carlos.andres_paez@outlook.com>
2021-05-13 22:11:39 -0500

    Indice en toStringClasses

--- RPG/src/rpg/RPG.java
+++ RPG/src/rpg/RPG.java
@@ -180,8 +180,9 @@ public abstract class RPG <T>{
 	}
 	public String toStringClasses() {
 		String str = "";
+		int j = 1;
 		for (PlayerClass i: this.classes)
-			str += i.toString() + "\n";
+			str += j + ") " + i.toString() + "\n";
 		return str;
 	}
 	public String toStringSkills() {
